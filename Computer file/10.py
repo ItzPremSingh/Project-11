305a90cc466299308fbe58182b77669239ff31e7
Prem Singh <itzpremsingh@duck.com>
2024-12-23 16:55:04 +0530

    Updating output

--- Computer file/10.py
+++ Computer file/10.py
@@ -12,6 +12,14 @@ volume = length * breadth * height
 print("Area of cuboid is", area)
 print("Volume of cuboid is", volume)
 
+# Output
+
+# Enter the length: 10
+# Enter the breadth: 5
+# Enter the height: 2
+# Area of cuboid is 100
+# Volume of cuboid is 10
+
 
 # Cube
 
@@ -21,8 +29,15 @@ volume = side * side * side
 print("Area of cube is", area)
 print("Volume of cube is", volume)
 
+# Output
+
+# Enter the side: 5
+# Area of cube is 150
+# Volume of cube is 125
+
 
 # Cylinder
+
 pi = 3.14
 
 radius = float(input("Enter the radius: "))
@@ -32,13 +47,27 @@ volume = pi * radius * radius * height
 print("Area of cylinder is", area)
 print("Volume of cylinder is", volume)
 
+# Output
+
+# Enter the radius: 5
+# Enter the height: 10
+# Area of cylinder is 62.83
+# Volume of cylinder is 314.15
+
 
 # Cone
 
 radius = float(input("Enter the radius: "))
 height = float(input("Enter the height: "))
-slant = (radius**2 + height**2) ** 0.5
-area = pi * radius * (radius + slant)
+slant_height = (radius**2 + height**2) ** 0.5
+area = pi * radius * (radius + slant_height)
 volume = (pi * radius * radius * height) / 3
 print("Area of cone is", area)
 print("Volume of cone is", volume)
+
+# Output
+
+# Enter the radius: 5
+# Enter the height: 10
+# Area of cone is 31.42
+# Volume of cone is 16.67
