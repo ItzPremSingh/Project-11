5f2125a11e0c4173c3ef4e7e7d9ccef98ede3339
Prem Singh <itzpremsingh@duck.com>
2024-12-26 20:38:06 +0530

    Update 5.py

--- Computer file/5.py
+++ Computer file/5.py
@@ -1,32 +1,32 @@
 # Write a program in python to check entered number is prime number or not.
 # With using for and while loop.
 
-number = int(input("Enter a number to check if it is prime: "))
+n = int(input("Enter a number to check if it is prime: "))
 
 print("Checking using for loop:")
-if number > 1:
-    for i in range(2, int(number**0.5) + 1):
-        if number % i == 0:
-            print(f"{number} is not a prime number.")
+if n > 1:
+    for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+            print(f"{n} is not a prime number.")
             break
     else:
-        print(f"{number} is a prime number.")
+        print(f"{n} is a prime number.")
 else:
-    print(f"{number} is not a prime number.")
+    print(f"{n} is not a prime number.")
 
 
 print("Checking using while loop:")
-if number > 1:
+if n > 1:
     i = 2
-    while i <= int(number**0.5):
-        if number % i == 0:
-            print(f"{number} is not a prime number.")
+    while i <= int(n**0.5):
+        if n % i == 0:
+            print(f"{n} is not a prime number.")
             break
         i += 1
     else:
-        print(f"{number} is a prime number.")
+        print(f"{n} is a prime number.")
 else:
-    print(f"{number} is not a prime number.")
+    print(f"{n} is not a prime number.")
 
 
 # Output
